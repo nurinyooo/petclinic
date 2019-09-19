f1509ecf423249c87bf3bcec78b8cb6ba7769d78
Nurinyooo <nurullahdur@gmail.com>
2019-09-19 14:47:57 +0300

    add new Files

---
+++ petclinicdata/src/main/java/com/nurinyooo/petclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.nurinyooo.petclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ petclinicdata/src/main/java/com/nurinyooo/petclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package com.nurinyooo.petclinic.model;
+
+public class Vet extends Person {
+
+
+}
