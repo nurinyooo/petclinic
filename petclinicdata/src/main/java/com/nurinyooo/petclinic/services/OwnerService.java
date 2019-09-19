c9ec91ec36d4bb37c0cb09159ec03adaccd383f4
Nurinyooo <nurullahdur@gmail.com>
2019-09-19 16:18:00 +0300

    CrudService was added and Other services was updated

--- petclinicdata/src/main/java/com/nurinyooo/petclinic/services/OwnerService.java
+++ petclinicdata/src/main/java/com/nurinyooo/petclinic/services/OwnerService.java
@@ -2,18 +2,7 @@ package com.nurinyooo.petclinic.services;
 
 import com.nurinyooo.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService  extends CrudService<Owner,Long>{
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
-
-
 }
--- petclinicdata/src/main/java/com/nurinyooo/petclinic/services/PetService.java
+++ petclinicdata/src/main/java/com/nurinyooo/petclinic/services/PetService.java
@@ -3,13 +3,6 @@ package com.nurinyooo.petclinic.services;
 
 import com.nurinyooo.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet,Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
