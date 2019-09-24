b69c69577f3c044be49dc8b800c6c00edd92ce04
Nurinyooo <nurullahdur@gmail.com>
2019-09-24 03:15:50 +0300

    Base Entity MappedSuperclass annotation was added

--- petclinicdata/src/main/java/com/nurinyooo/petclinic/model/BaseEntity.java
+++ petclinicdata/src/main/java/com/nurinyooo/petclinic/model/BaseEntity.java
@@ -1,10 +1,17 @@
 package com.nurinyooo.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-    public class BaseEntity  implements Serializable {
+@MappedSuperclass
+public class BaseEntity  implements Serializable {
 
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     public Long getId() {
         return id;
